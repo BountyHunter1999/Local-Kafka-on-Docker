677c791c4fa4165c2f86538cfd6cf8c90542390a
shrish <adhikari.shrish1@gmail.com>
2023-12-19 21:12:00 +0545

    Working single node, single topic and partition example

--- src/producer.py
+++ src/producer.py
@@ -1,4 +1,5 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaAdminClient
+from kafka.admin.new_partitions import NewPartitions
 import json
 from csv import DictReader
 
@@ -6,6 +7,20 @@ from csv import DictReader
 def is_valid():
     # tells if the link is valid
     pass
+
+
+def key_divider(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+# def get_producer() -> KafkaProducer:
+#     client = KafkaAdminClient(bootstrap_servers="kafka")
+#     topic_name = "linkedin"
+#     new_partition_count = 2
+
+#     rsp = client.create_partitions({"linkedin": NewPartitions(2)})
+#     return client
 
 
 def get_producer() -> KafkaProducer:
@@ -37,7 +52,10 @@ if __name__ == "__main__":
     with open("data.csv") as f:
         csv_dict_reader = DictReader(f)
         for row in csv_dict_reader:
-            ack = producer.send(topic="linkedin", value=json.dumps(row).encode("utf-8"))
+            ack = producer.send(
+                topic="linkedin",
+                value=json.dumps(row).encode("utf-8"),
+            )
             metadata = ack.get()
 
             print(metadata.topic, metadata.partition, metadata)
